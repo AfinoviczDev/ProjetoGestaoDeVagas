47547ee0035eb1d3b0e2c293ad5b5fd6bf56b5de
AfinoviczDev <jpafinovicz@gmail.com>
2026-06-22 17:53:39 -0300

    criando CandidateService e formatando mensagem de erro para usuário ja existente

---
+++ src/main/java/dev/afinovicz/GestaoDeVagas/useCases/CandidateService.java
@@ -0,0 +1,23 @@
+package dev.afinovicz.GestaoDeVagas.useCases;
+
+import dev.afinovicz.GestaoDeVagas.database.model.CandidateEntity;
+import dev.afinovicz.GestaoDeVagas.database.repository.CandidateRepository;
+import dev.afinovicz.GestaoDeVagas.exception.UserFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CandidateService {
+
+    @Autowired
+    private CandidateRepository candidateRepository;
+
+    public CandidateEntity execute(CandidateEntity candidateEntity) {
+        this.candidateRepository.findByUsernameOrEmail(candidateEntity.getUsername(), candidateEntity.getEmail())
+                .ifPresent((user) -> {
+                    throw new UserFoundException();
+                });
+        return this.candidateRepository.save(candidateEntity);
+    }
+
+}
